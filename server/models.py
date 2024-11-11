6124ee30a743e67704304c10083f602575296d22
Tess <tess.mueske@gmail.com>
2024-11-11 14:56:24 +0100

    adding models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,18 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+class Traveler(db.Model, SerializerMixin):
+     __tablename__ = 'travelers'
+
+class Island(db.Model, SerializerMixin):
+     __tablename__ = 'islands'
+
+class Guide(db.Model, SerializerMixin):
+     __tablename__ = 'guides'
+
+class LocalExpert(db.Model, SerializerMixin):
+     __tablename__ = 'localexperts'
+
+class Advertiser(db.Model, SerializerMixin):
+     __tablename__ = 'advertisers'
