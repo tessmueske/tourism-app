20703463161302b0884d05456e2f9a38363d67e1
Tess <tess.mueske@gmail.com>
2024-11-14 15:30:37 +0000

    more css

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -3,11 +3,11 @@ import '../index.css';
 
 function Homepage(){
   return (
-    <div className='center-container'>
+    <div>
         <h1 className='home-h1'>welcome to magwa</h1>
         <h2 className='home-h2'>⋇⊶⊰❣⊱⊷⋇</h2>
-        <h2>we are an independent organization that promotes culturally and ecologically sensitive tourism in the canary islands.</h2>
-        <p>click here to learn more about magwa.</p>
+        <h2 className='center-container'>we are an independent organization that promotes culturally and ecologically sensitive tourism in the canary islands.</h2>
+        <p className='center-container'>click here to learn more about magwa.</p>
     </div>
   );
 };
