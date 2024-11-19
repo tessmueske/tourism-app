04a3bacf4679804ee7142483ddc07a4fbf7f3993
Tess <tess.mueske@gmail.com>
2024-11-19 19:19:48 +0100

    css fixes

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,15 +1,19 @@
 import React from "react";
 import '../index.css'; 
 
-function Homepage(){
+function Homepage() {
   return (
     <div>
-        <h1 className='home-h1'>welcome to magwa</h1>
-        <h2 className='home-h2'>⋇⊶⊰❣⊱⊷⋇</h2>
-        <h2 className='center-container'>we are an independent organization that promotes culturally and ecologically sensitive tourism in the canary islands.</h2>
-        <p className='center-container'>click here to learn more about magwa.</p>
+      <div className="home-header">
+        <h1 className="home-h1">welcome to magwa</h1>
+        <h2 className="home-h2">⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇</h2>
+      </div>
+      <div className="center-container">
+        <h2>we are an independent organization that promotes culturally and ecologically sensitive tourism in the canary islands.</h2>
+        <p>click here to learn more about magwa.</p>
+      </div>
     </div>
   );
-};
+}
 
 export default Homepage;
