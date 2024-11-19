1703c9436faf1e491f4a14c34a18a5c993bba9ec
Tess <tess.mueske@gmail.com>
2024-11-19 19:31:47 +0100

    text additions

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,6 +5,7 @@ function About(){
   return (
     <div className='center-container'>
         <h1>magwa (pronounced mah-wa)</h1>
+        <h2>⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇</h2>
         <p>magwa is a traditional word from the canary islands that means disappointment, grief, or sadness.</p>
         <p>have you been wanting to visit the canary islands but want to ensure you are not contributing to the growing number of those in poverty, foreign hotel conglomerates paying their canarian employees below minimum wage, and ecological disasters?</p>
         <p>we are working to solve the tourism crisis in the canary islands. </p>
