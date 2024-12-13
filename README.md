5dac524759971295516e4a85420e831b7768dfcc
Tess <tess.mueske@gmail.com>
2024-12-13 14:29:49 +0100

    Updated about page

--- README.md
+++ README.md
@@ -1,40 +1,4 @@
-Definition of magwa: https://hdl.handle.net/11730/guatc/2224
-
 ################
-
-Tourism has ravaged the Canary Islands, leading to a reduction in available housing for local inhabitants.
-In 2023 over 16 million tourists visited
-Provides jobs and money for the local economy, but at what cost? 
-"As many properties are owned by second home-owners who don't live there all the time, buying a property to live in has become prohibitively expensive - and renting an affordable apartment has long been a struggle."
-The biodiversity has been affected
-
-No tourist tax, no limitations, no actual legislative change.
-
-https://www.euronews.com/my-europe/2024/10/20/thousands-protest-against-over-tourism-in-canary-islands#:~:text=Tourism%20is%20the%20main%20economic,is%20damaging%20the%20environment%20too.&text=Around%2030%2C000%20people%20took%20to,Canary%20Islands%20have%20a%20limit.%22
-
-##################
-
-Completely unaffordable for residents, strain on resources
-
-"The protests, which are taking place under the banner “Canarias tiene un límite” – The Canaries have a limit – are backed by environmental groups including Greenpeace, WWF, Ecologists in Action, Friends of the Earth and SEO/Birdlife."
-
-Luxury developments --> hunger strikes from local citizens and organizers for Canarias se Agote (The Canaries have had enough)
-
-Tourism is 35$ of the GDP of the Canarias
-
-33.8% of the island's inhabitants are at risk of poverty or social exclusion
-
-Water emergency
-
-"high prices, low wages, a lack of public housing and the continuing cost of living crisis"
-
-"“The problem isn’t the tourists,” he said. “It’s a model that was built around, and with the connivance of, a business class that doesn’t want to listen to what needs to be done, and with a political class that serves that business class instead of serving all the citizens.”"
-
-https://www.theguardian.com/world/2024/apr/20/thousands-protest-canary-islands-unsustainable-tourism
-
-##################
-
-https://canariaseagota.com/
 
 
 
@@ -42,4 +6,5 @@ https://canariaseagota.com/
 # Community calendar
 # Trip planner
 # Friendship capabilities
-# see only posts by (travelers/local experts/advertisers), filter by hashtags
+# In-app message capabilities
+# see only posts by (travelers/local experts/advertisers)
--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,19 +6,26 @@ function About(){
     <div className='about-center-container'>
         <h1>magwa <span className="italic">(MAH-wah)</span></h1>
         <h2>⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇</h2>
-        <p><span className="italic">magwa</span> is a traditional word from the canary islands that means disappointment, grief, or sadness.</p>
-        <p>have you been wanting to visit the canary islands but want to ensure you are not contributing to the growing number of those in poverty, foreign hotel conglomerates paying their canarian employees below minimum wage, and ecological disasters?</p>
+        <p><span className="italic">magwa</span> is a traditional word from the canary islands that means disappointment, grief, or sadness. tourism has ravaged the Canary Islands, leading to a reduction in available housing for local inhabitants, adverse environmental impacts, and other negative consequences. the islands have become completely unaffordable for residents, putting a strain on resources, leading to a housing and employment crisis. one-third of the residents of the canary islanda are at risk for homelessness and social exclusion, according to Instituo Nacional de Estadística, Spain's national statistics institute.</p>
+        <p>despite protests from the local residents, there has been no tourist tax, no limitations of any kind imposed upon foreigners, and no actual legislative change.</p>
+        <p>there are local groups who organize protests such as large public manifestations, vigils, and hunger strikes to raise awareness about the problem. one such local group, Canarias Se Agota, or Canarians are Sold Out, operate under the motto "Nuestros Cuerpos por Nuestra Tierra" -- Our Bodies for Our Land. the british newspaper The Guardian reports that the protests are are backed by various environmental groups, such as Greenpeace, WWF, Ecologists in Action, Friends of the Earth, and SEO/Birdlife. a spokesperson for Carnarias Se Agota, Víctor Martín, told the Guardian, <span className="italic">“The problem isn’t the tourists...[i]t’s a model that was built around, and with the connivance of, a business class that doesn’t want to listen to what needs to be done, and with a political class that serves that business class instead of serving all the citizens.”</span></p>
+        <p>however, the collection of eight islands do depend on tourism for a substantial amount of their income.</p>
+        <p>how does one visit the canary islands but ensure they are not contributing to the growing number of those in poverty, foreign hotel conglomerates paying their canarian employees below minimum wage, and ecological disasters?</p>
         <h2>we are working to solve the tourism crisis in the canary islands. </h2>
-        <p>click here for more research about tourism in the canary islands.</p>
         <h2>⋇⊶⊰❣⊱⊷⋇ so, what's this app? ⋇⊶⊰❣⊱⊷⋇</h2>
-        <p>the functionality of magwa is twofold: it serves as a personal travel planning application, and it's a community hub for Q&A, discussion, events, and businesses and services.</p>
-        <p>to use magwa, you can sign up as a traveler, a local expert, or an advertiser. you cannot sign up as more than one type of user.</p>
+        <p>magwa serves as a social media network and community hub for Q&A, discussion, events, and businesses and services -- while keeping it all local. we are built by and for the people! we have a public discussion board where users can post their questions and respond to one another, as well as visit each others profiles to get in touch with one another.</p>
+        <p>to use magwa, you can sign up as a traveler, a local expert, or an advertiser. you cannot sign up as more than one type of user. adverisers and local experts must tell us a bit about themselves before use of the site, in order to ensure every person who uses the application is truly local.</p>
         <h2>⋇⊶⊰❣⊱⊷⋇ am i a traveler, local expert, or advertiser? ⋇⊶⊰❣⊱⊷⋇</h2>
         <p>you are a <span className="italic">traveler</span> if you are visiting the island and want to browse local businesses, events, and connect with locals. you are a <span className="italic">local expert</span> if you live in or are from the canary islands and want to meet travelers, post your events, or engage in community discussion. you are an <span className="italic">advertiser</span> if you are from or live in the canary islands and have a business or service you want to promote.</p>
-        <p>every person who signs up as a local expert or advertiser will be vetted by our board to ensure the application stays local.</p>
         <p>click on the 'sign up' button above to make an account and start investing in local businesses!</p>
         <h2>⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇⋇⊶⊰❣⊱⊷⋇</h2>
-        <p>image credits: </p>
+        <p style={{ fontSize: '14px' }}>sources: </p>
+        <p style={{ fontSize: '12px' }}>https://www.euronews.com/my-europe/2024/10/20/thousands-protest-against-over-tourism-in-canary-islands#:~:text=Tourism%20is%20the%20main%20economic,is%20damaging%20the%20environment%20too.&text=Around%2030%2C000%20people%20took%20to,Canary%20Islands%20have%20a%20limit.%22</p>
+        <p style={{ fontSize: '12px' }}>https://www.theguardian.com/world/2024/apr/20/thousands-protest-canary-islands-unsustainable-tourism</p>
+        <p style={{ fontSize: '12px' }}>https://www.ine.es/dyngs/Prensa/es/ECV2023.htm</p>
+        <p style={{ fontSize: '12px' }}>https://canariaseagota.com/</p>
+        <br></br>
+        <br></br>
     </div>
   );
 };
